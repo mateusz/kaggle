4a32471985a1731916cbe02aa31b0062a94ca869
mateusz <m@emptyshore.org>
2023-04-24 15:29:33 +1200

    stars still missing

--- compress/src/01_fist_go.py
+++ compress/src/01_fist_go.py
@@ -66,36 +66,43 @@ latent = 256
 net = nn.Sequential(
         nn.Conv2d(3, ch, 5),
         nn.MaxPool2d(2),
-        nn.ReLU(inplace=True),
+        GDN(ch, device),
+        torch.nn.GroupNorm(1,ch),
+        #nn.BatchNorm2d(ch),
         
         nn.Conv2d(ch, ch*2, 3),
         nn.MaxPool2d(2),
-        nn.ReLU(inplace=True),
-        nn.BatchNorm2d(ch*2),
+        GDN(ch*2, device),
+        torch.nn.GroupNorm(1,ch*2),
+        #nn.BatchNorm2d(ch*2),
         # 32,62,62
 
         nn.Conv2d(ch*2, ch*4, 3),
         nn.MaxPool2d(2),
         nn.ReLU(inplace=True),
-        nn.BatchNorm2d(ch*4),
+        torch.nn.GroupNorm(1,ch*4),
+        #nn.BatchNorm2d(ch*4),
         # 64,30,30
 
         nn.Conv2d(ch*4, ch*8, 3),
         nn.MaxPool2d(2),
         nn.ReLU(inplace=True),
-        nn.BatchNorm2d(ch*8),
+        torch.nn.GroupNorm(1,ch*8),
+        #nn.BatchNorm2d(ch*8),
         #128,14,14
 
         nn.Conv2d(ch*8, ch*16, 3),
         nn.MaxPool2d(2),
         nn.ReLU(inplace=True),
-        nn.BatchNorm2d(ch*16),
+        torch.nn.GroupNorm(1,ch*16),
+        #nn.BatchNorm2d(ch*16),
         # 256,6,6
 
         nn.Conv2d(ch*16, ch*32, 3),
         nn.MaxPool2d(2),
         nn.ReLU(inplace=True),
-        nn.BatchNorm2d(ch*32),
+        torch.nn.GroupNorm(1,ch*32),
+        #nn.BatchNorm2d(ch*32),
         # 512,2,2
 
         nn.Flatten(),
@@ -107,23 +114,28 @@ net = nn.Sequential(
 
         nn.ConvTranspose2d(ch*32, ch*16, 4, stride=2),
         nn.ReLU(inplace=True),
-        nn.BatchNorm2d(ch*16),
+        torch.nn.GroupNorm(1,ch*16),
+        #nn.BatchNorm2d(ch*16),
 
         nn.ConvTranspose2d(ch*16, ch*8, 4, stride=2),
         nn.ReLU(inplace=True),
-        nn.BatchNorm2d(ch*8),
+        torch.nn.GroupNorm(1,ch*8),
+        #nn.BatchNorm2d(ch*8),
 
         nn.ConvTranspose2d(ch*8, ch*4, 4, stride=2),
         nn.ReLU(inplace=True),
-        nn.BatchNorm2d(ch*4),
+        torch.nn.GroupNorm(1,ch*4),
+        #nn.BatchNorm2d(ch*4),
 
         nn.ConvTranspose2d(ch*4, ch*2, 4, stride=2),
-        nn.ReLU(inplace=True),
-        nn.BatchNorm2d(ch*2),
+        GDN(ch*2, device),
+        torch.nn.GroupNorm(1,ch*2),
+        #nn.BatchNorm2d(ch*2),
 
         nn.ConvTranspose2d(ch*2, ch, 4, stride=2),
-        nn.ReLU(inplace=True),
-        nn.BatchNorm2d(ch),
+        GDN(ch, device),
+        torch.nn.GroupNorm(1,ch),
+        #nn.BatchNorm2d(ch),
 
         nn.ConvTranspose2d(ch, 3, 6, stride=2),
 )
@@ -131,9 +143,10 @@ net = nn.Sequential(
 net = net.to(device)
 print(net(iset[0].unsqueeze(0)).shape)
 print(sum([l.numel() for l in net.parameters()]))
+min_loss = 9999999
 #%%
 
-opt = torch.optim.Adam(net.parameters(), lr=0.0001)
+opt = torch.optim.Adam(net.parameters(), lr=0.001)
 
 perc = SSIM(data_range=1, size_average=True, channel=3)
 abs = nn.L1Loss()
@@ -143,6 +156,8 @@ abs_w = 1.0
 
 net = net.to(device)
 net.train()
+early_stop = 0
+report_steps = 10
 for epoch in range(999999):
     running_loss = 0.0
     running_perc = 0.0
@@ -164,13 +179,26 @@ for epoch in range(999999):
         running_abs += abs_loss.item()
         running_perc += perc_loss.item()
 
-    print("[%d] l=%.4f, abs=%.4f, perc=%.4f" % (epoch, running_loss, running_abs, running_perc))
+    if epoch%report_steps==(report_steps-1):
+        print("[%d] l=%.4f, abs=%.4f, perc=%.8f (min=%.4f)" % (epoch, running_loss, running_abs, running_perc, min_loss))
+
+    if running_loss<min_loss:
+        early_stop = 0
+        min_loss = running_loss
+        if running_loss<0.02:
+            torch.save(net, 'models/01')
+    else:
+        early_stop += 1
+
+    if early_stop>250:
+        break
+
     running_loss = 0.0
     running_abs = 0.0
     running_perc = 0.0
 
 #%%
-
+net = torch.load('models/01')
 net.eval()
 
 def show_img(iset, data):
@@ -188,3 +216,5 @@ def show_img(iset, data):
 
 show_img(iset, iset[:])
 show_img(iset, net(iset[:]))
+
+# about [329] l=0.0169, abs=0.0168, perc=0.00006306 (min=0.0167)
